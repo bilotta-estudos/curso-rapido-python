493f88289cacaa175850106e15ff6fd1fffefa4d
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2021-10-25 13:53:20 -0300

    AULA 03 - Tipos básicos

---
+++ tipos-basicos.py
@@ -0,0 +1,22 @@
+#Utilizando a função type() para saber o tipo das variáveis
+
+#String
+print(type('Teste'))
+
+#Inteiro
+print(type(1))
+
+#Float
+print(type(1.1))
+
+#Booleano
+print(type(True))
+
+#Booleano
+print(type(False))
+
+#Lista (Similar ao array em javascript)
+print(type([1, 2, 3]))
+
+#Dicionário (Similar ao objeto no javascript)
+print(type({'nome': 'João', 'idade': 23}))
