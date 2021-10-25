0fb32bfa0f44f483895825e09bc80c67a2108608
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2021-10-25 13:36:01 -0300

    AULA 02 - Comentários

---
+++ comentarios.py
@@ -0,0 +1,6 @@
+#Isso é um comentário de uma linha, utilizamos o # (hashtag)
+
+""" Isso é um comentário
+de múltiplas linhas 
+(Utilizamos três aspas no início e no final do texto,
+podendo utilizar aspas simples também)"""
